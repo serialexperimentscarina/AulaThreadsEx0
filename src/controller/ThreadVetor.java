5bbc99486d300f10178514d1faa11579cb519943
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-03 17:16:54 -0300

    feat: Create ThreadVetor

--- src/controller/ThreadVetor.java
+++ src/controller/ThreadVetor.java
@@ -12,7 +12,39 @@ public class ThreadVetor extends Thread{
 	
 	@Override
 	public void run() {
+		if (valor % 2 == 0) {
+			par();
+		} else {
+			impar();
+		}
+	}
+
+	private void par() {
+		double tempoInicial = System.nanoTime();
+		long soma = 0;
 		
+		for (int i = 0; i < vet.length; i++) {
+			soma += vet[i];
+		}
+		
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - tempoInicial;
+		tempoTotal /= Math.pow(10, 9);
+		System.out.println("Tempo para percorrer vetor com for: " + tempoTotal + "s.");
 	}
 	
+	private void impar() {
+		double tempoInicial = System.nanoTime();
+		long soma = 0;
+		
+		for (int i : vet) {
+			soma += i;
+		}
+		
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - tempoInicial;
+		tempoTotal /= Math.pow(10, 9);
+		System.out.println("Tempo para percorrer vetor com foreach: " + tempoTotal + "s.");
+	}
+
 }
